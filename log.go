6558b267ac44407db4fba81361819f675bcd03e5
Matt W. <human@mattgonewild.com>
2025-07-06 15:37:16 -0500

    add NewBoundedCursor to Log[T]

--- log.go
+++ log.go
@@ -1,7 +1,10 @@
 package common
 
+import "time"
+
 type Log[T Timestamped] interface {
 	Append(element T) error
 	Tail() (T, bool)
 	NewCursor() Cursor[T]
+	NewBoundedCursor(through time.Time) Cursor[T]
 }
