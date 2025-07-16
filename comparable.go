f42a2b1542b98d8817a946a851ce4e4dbbd28b49
Matt W. <human@mattgonewild.com>
2025-07-16 00:14:42 -0500

    refactor Comparable interface to avoid dispatcher and encourage good design

--- comparable.go
+++ comparable.go
@@ -1,5 +1,8 @@
 package common
 
-type Comparable interface {
-	Compare(comparable Comparable) int
+type Comparable[T any] interface {
+	Before(comparable T) bool
+	Equal(comparable T) bool
+	After(comparable T) bool
+	Compare(comparable T) int
 }
--- queue.go
+++ queue.go
@@ -1,9 +1,10 @@
 package common
 
-type Queue[T Comparable] interface {
+type Queue[T Comparable[T]] interface {
 	Push(element T) bool
 	Pop() (T, bool)
 	Peek() (T, bool)
 	Remove(element T) bool
 	Len() int
+	Cap() int
 }
