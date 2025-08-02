0c3b6e95fdd47ad690720ceeaa1212c2212b5377
Matt W. <human@mattgonewild.com>
2025-08-02 10:10:38 -0500

    update Log interface to include start time parameters for NewCursor methods

--- log.go
+++ log.go
@@ -5,6 +5,6 @@ import "time"
 type Log[T Timestamped] interface {
 	Append(element T) error
 	Tail() (T, bool)
-	NewCursor() Cursor[T]
-	NewBoundedCursor(through time.Time) Cursor[T]
+	NewCursor(start time.Time) Cursor[T]
+	NewBoundedCursor(start, through time.Time) Cursor[T]
 }
