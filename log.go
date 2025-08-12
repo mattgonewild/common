2b89e86ed4d6f9fc805cefe79ed0540af28a2a55
Matt W. <human@mattgonewild.com>
2025-08-12 13:27:39 -0500

    update Log interface to modify NewCursor method signature with anchor and end parameters; remove NewBoundedCursor

--- log.go
+++ log.go
@@ -5,6 +5,5 @@ import "time"
 type Log[T Timestamped] interface {
 	Append(element T) error
 	Tail() (T, bool)
-	NewCursor(start time.Time) (Cursor[T], error)
-	NewBoundedCursor(start, through time.Time) (Cursor[T], error)
+	NewCursor(anchor time.Time, end bool) (Cursor[T], error)
 }
