dad783f80eb5267cc77700e13fae5a7631def0bb
Matt W. <human@mattgonewild.com>
2025-08-13 11:33:47 -0500

    update Log interface to replace NewCursor method with NewCursorBefore, NewCursorAt, and NewCursorAfter methods

--- log.go
+++ log.go
@@ -5,5 +5,8 @@ import "time"
 type Log[T Timestamped] interface {
 	Append(element T) error
 	Tail() (T, bool)
-	NewCursor(anchor time.Time, end bool) (Cursor[T], error)
+
+	NewCursorBefore(end time.Time) (Cursor[T], error)
+	NewCursorAt(floor time.Time) (Cursor[T], error)
+	NewCursorAfter(start time.Time) (Cursor[T], error)
 }
