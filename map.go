2743e007ea871c71e4a0f6df032be5f20e4adcae
Matt W. <human@mattgonewild.com>
2025-08-23 18:28:40 -0500

    add SortedMap interface

--- map.go
+++ map.go
@@ -1,7 +1,16 @@
 package common
 
-type Map[T any] interface {
-	Set(key Key, value T) error
-	Get(key Key) (T, error)
+import "iter"
+
+type Map[V any] interface {
+	Set(key Key, value V) error
+	Get(key Key) (V, error)
 	Delete(key Key) error
+}
+
+type SortedMap[K Comparable[K], V any] interface {
+	Set(key K, value V) error
+	Get(key K) (V, error)
+	Delete(key K) error
+	All() iter.Seq2[K, V]
 }
